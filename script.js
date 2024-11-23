b05d1c93348e4200937f13a93657ff1eba27382d
KALYAN <manojkalyan27@gmail.com>
2024-11-23 19:46:50 +0530

    Updated Code

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const mail = document.querySelector("#mail");
 const word = document.querySelector("#password");
-const passwordSection = document.querySelector('#passwordSection')
 const validationMessage = document.querySelector(".validationMessage");
 const btn = document.querySelector('button');
+const errorMessage = document.querySelector(".error");
+const errorEmailMessage = document.querySelector(".emailError");
+
+
 
 let isEmail = false;
 let isPassword = false;
 
 
 mail.addEventListener("change",()=>{
-const errorEmailMessage = document.querySelector(".emailError");
 let Email = mail.value;
 let isChar = false;
 if((Email.split('@')[0].length > 3) && (/[a-zA-Z]/.test(Email.split('@')[0]))){
@@ -17,6 +19,7 @@ if((Email.split('@')[0].length > 3) && (/[a-zA-Z]/.test(Email.split('@')[0]))){
 }
     if(!(Email.includes("@") && Email.includes(".") && isChar)){
         errorEmailMessage.style.display = 'block';
+        isEmail = false;  
     }else{
         errorEmailMessage.style.display ='none';  
         isEmail = true;  
@@ -24,9 +27,9 @@ if((Email.split('@')[0].length > 3) && (/[a-zA-Z]/.test(Email.split('@')[0]))){
 })
 
 word.addEventListener('change', (e)=>{
-const errorMessage = document.querySelector(".error");
     if(e.target.value.length < 8){           
         errorMessage.style.display ='block';
+        isPassword = false;
     }else if(e.target.value.length >=8){
         errorMessage.style.display ='none'; 
         isPassword = true;
@@ -38,10 +41,22 @@ const errorMessage = document.querySelector(".error");
 })
 
 btn.addEventListener('click',()=>{
-    let result = confirm("Press OK to proceed");
+    
+    if(isEmail && isPassword){
+        // validationMessage.style.display='block';
+        
+        let result = confirm("Press OK to proceed");
             if (result === true) {
                 alert("successful signup!");
             }
+    }else{
+        if(!(isEmail)){
+            errorEmailMessage.style.display = 'block';
+        }
+        else if(!(isPassword)){
+            errorMessage.style.display ='block';
+        }
+    }
 });
 
 
